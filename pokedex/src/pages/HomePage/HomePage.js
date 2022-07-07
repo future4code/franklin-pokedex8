c4e7902f469f22db87e4323f8eb115ba85dcb881
Nicole Rafaella Vieira Peiker <nicolervpeiker@gmail.com>
2022-07-07 13:56:44 -0300

    requisição da lista de pokemons

--- pokedex/src/pages/HomePage/HomePage.js
+++ pokedex/src/pages/HomePage/HomePage.js
@@ -1,29 +1,44 @@
 import React from 'react';
+import { BASE_URL } from '../../constants/url';
+import { useRequestData } from '../../hooks/useRequestData';
 
-const HomePage = () => {
-  
-  return(
-  <div>
-    <div className='Header'>
-      <button>
-        pokedex
-      </button>
-      Lista Pokemons
-    </div>
-    <div>
-      {/* <div className='Poke Card'>
-        <img src='###' />
+const HomePage = ({ pokedex, setPokedex }) => {
+  const [getPokemons] = useRequestData(BASE_URL);
+  console.log(getPokemons.results);
+
+  const addPokedex = pokemon => {
+    const newPokedex = [...pokedex, pokemon];
+    setPokedex(newPokedex);
+    console.log(pokedex);
+  };
+
+  const pokeCard = getPokemons.results.map(pokemon => {
+    return (
+      <div className="Poke Card">
+        <img
+          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+            pokemon.url.split('/')[6]
+          }.svg`}
+          alt={pokemon.name}
+        />
         <div>
-          <h2>nomePokemon</h2>                      // Card na requisição
+          <h2>{pokemon.name}</h2>
           <div>
-            <button> Adicionar </button>
+            <button onClick={addPokedex}> Adicionar </button>
             <button> Detalhes </button>
           </div>
         </div>
-      </div> */}
+      </div>
+    );
+  });
+  return (
+    <div>
+      <div className="Header">
+        <button>pokedex</button>
+        Lista Pokemons
+      </div>
+      <div>{getPokemons && pokeCard}</div>
     </div>
-
-  </div>
   );
 };
 
