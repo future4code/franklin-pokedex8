391bb07daa7a411cc0418f9f884a6a334d88ce9a
Nicole Peiker <65312009+nicolepeiker@users.noreply.github.com>
2022-07-07 21:58:49 -0300

    Delete pokedex directory

--- pokedex/src/routes/coordinator.js
+++
@@ -1,9 +0,0 @@
-export const goToHomePage = navigate => {
-  navigate('/');
-};
-export const goToPokedexPage = navigate => {
-  navigate(-1);
-};
-export const goToDetailsPage = (navigate, id) => {
-  navigate(`/${idOrName}`);
-};
